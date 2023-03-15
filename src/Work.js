962ee8b72d743ca05459d57d33e4c9ec980e272c
Abdul1rhman <pain008900@gmail.com>
2023-03-16 02:41:30 +0300

    fix some styles

--- src/Work.js
+++ src/Work.js
@@ -6,6 +6,8 @@ import { Col, Row } from 'react-bootstrap';
 import fir from './firstweb.PNG'
 import athkar from './athkar.PNG'
 import sec from './secweb.PNG' 
+import post from './Post.PNG' 
+
 import {BsCodeSlash} from 'react-icons/bs';
 import {AiOutlineArrowLeft}from 'react-icons/ai';
 function Work({lan}) {
@@ -36,14 +38,14 @@ function Work({lan}) {
 
             <Col >
                 <Card className='card' >
-                    <Card.Img variant="top" src={fir} />
+                    <Card.Img variant="top" src={post} />
                     <Card.Body>
-                    <Card.Title style={{marginBottom:'30px'}}>الموقع الاول</Card.Title>
+                    <Card.Title style={{marginBottom:'30px'}}>المدونات</Card.Title>
                     {/* <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                     </Card.Text> */}
-                    <a href='https://abdul1rhman.github.io/'target='_blank' variant="dark" className="btn btn-dark">زيارة الموقع <AiOutlineArrowLeft/></a>
+                    <a href='https://posts-sh.vercel.app/'target='_blank' variant="dark" className="btn btn-dark">زيارة الموقع <AiOutlineArrowLeft/></a>
                     </Card.Body>
                 </Card>
             </Col>
@@ -72,12 +74,12 @@ function Work({lan}) {
                 <Card className='card' >
                     <Card.Img variant="top" src={fir} />
                     <Card.Body>
-                    <Card.Title style={{marginBottom:'30px'}}>الموقع الثاني</Card.Title>
+                    <Card.Title style={{marginBottom:'30px'}}>الموقع الاول</Card.Title>
                     {/* <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                     </Card.Text> */}
-                    <a href='https://abdul1rhman.github.io/athkar/'target='_blank' variant="dark" className="btn btn-dark">زيارة الموقع <AiOutlineArrowLeft/></a>
+                    <a href='https://abdul1rhman.github.io/'target='_blank' variant="dark" className="btn btn-dark">زيارة الموقع <AiOutlineArrowLeft/></a>
                     </Card.Body>
                 </Card>
             </Col>
@@ -108,15 +110,15 @@ function Work({lan}) {
             </Col>
 
             <Col >
-                <Card className='card' >
-                    <Card.Img variant="top" src={fir} />
+            <Card className='card' >
+                    <Card.Img variant="top" src={post} />
                     <Card.Body>
-                    <Card.Title style={{marginBottom:'30px'}}>Firt Website</Card.Title>
+                    <Card.Title style={{marginBottom:'30px'}}>Posts</Card.Title>
                     {/* <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                     </Card.Text> */}
-                    <a href='https://abdul1rhman.github.io/'target='_blank' variant="dark" className="btn btn-dark"><AiOutlineArrowLeft/> Visit Now</a>
+                    <a href='https://posts-sh.vercel.app/'target='_blank' variant="dark" className="btn btn-dark"><AiOutlineArrowLeft/> Visit Now</a>
                     </Card.Body>
                 </Card>
             </Col>
@@ -131,7 +133,7 @@ function Work({lan}) {
                 <Card className='card' >
                     <Card.Img variant="top" src={sec} />
                     <Card.Body>
-                    <Card.Title style={{marginBottom:'30px'}}>  </Card.Title>
+                    <Card.Title style={{marginBottom:'30px'}}>Second website</Card.Title>
                     {/* <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
@@ -145,12 +147,12 @@ function Work({lan}) {
                 <Card className='card' >
                     <Card.Img variant="top" src={fir} />
                     <Card.Body>
-                    <Card.Title style={{marginBottom:'30px'}}>Third Website</Card.Title>
+                    <Card.Title style={{marginBottom:'30px'}}>First Website</Card.Title>
                     {/* <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                     </Card.Text> */}
-                    <a href='https://abdul1rhman.github.io/athkar/'target='_blank' variant="dark" className="btn btn-dark"><AiOutlineArrowLeft/>  Visit Now</a>
+                    <a href='https://abdul1rhman.github.io/'target='_blank' variant="dark" className="btn btn-dark"><AiOutlineArrowLeft/>  Visit Now</a>
                     </Card.Body>
                 </Card>
             </Col>
