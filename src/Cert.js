02e95b437f67291405958ba92ab720ff722bed8a
Abdul1rhman <pain008900@gmail.com>
2023-10-15 02:26:09 +0300

    edit cert cards

--- src/Cert.js
+++ src/Cert.js
@@ -29,7 +29,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front">
                         <h2> Responsive Web Design</h2>
                         
-                        <p>Developer Certification, representing approximately 300 hours of coursework.</p>
                         <p>company: freeCodeCamp.org </p>
                         <span>flip the card to show more info</span>
                     </div>
@@ -48,7 +47,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front">
                         <h2>Azure Infrastructure Solutions</h2>
                         
-                        <p>Developer Certification, representing approximately 300 hours of coursework.</p>
                         <p>company: Microsoft and Misk</p>
                         <span>flip the card to show more info</span>
                     </div>
@@ -64,7 +62,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front">
                         <h2>Introduction to Cybersecurity</h2>
                         
-                        <p>This introductory course takes you inside the world of cybersecurity. You will learn cybersecurity basics to protect your personal digital life</p>
                         <p>company: Cisco</p>
                         <a className='badge' href='https://www.credly.com/badges/8b82addc-0047-4d79-a5c3-9d55235cbb95/public_url'target='_blank'>verify badge </a>
                         <span>flip the card to show more info</span>
@@ -82,7 +79,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front">
                         <h2>Career Ready Badge</h2>
                         
-                        <p>Proficiency in skills such as communication, project planning, and time management</p>
                         <p>company: Misk</p>
                         <a className='badge' href='https://www.credential.net/e2fce05f-25cb-40cf-9018-29ff6af565b2'target='_blank'>verify badge </a>
 
@@ -108,7 +104,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front" dir='rtl'>
                         <h2> تصميم الويب المتجاوب</h2>
                         
-                        <p>شهادة المطور، والتي تمثل حوالي 300 ساعة من الدورات الدراسية.</p>
                         <p>مقدمة من : freeCodeCamp.org </p>
                         <span>اقلب البطاقة لإظهار المزيد من المعلومات</span>
                     </div>
@@ -127,7 +122,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front" dir='rtl'>
                         <h2>Azure حلول البنية التحتية </h2>
                         
-                        <p>يمثل نظام Azure النظام الأساسي للخدمة السحابية العامة يدعم مجموعة واسعة من أنظمة التشغيل ولغات البرمجة وأطر العمل والأدوات وقواعد البيانات والأجهزة.</p>
                         <p>مقدمة من : Microsoft and Misk</p>
                         <span>اقلب البطاقة لإظهار المزيد من المعلومات</span>
                     </div>
@@ -143,7 +137,6 @@ const Card = ({lan}) => {
                     <div className="card-cert-front" dir='rtl'>
                         <h2>مقدمة في الأمن السيبراني</h2>
                         
-                        <p>تأخذك هذه الدورة التمهيدية إلى داخل عالم الأمن السيبراني. سوف تتعلم أساسيات الأمن السيبراني لحماية حياتك الرقمية </p>
                         <p>مقدمة من : Cisco</p>
                         <a className='badge' href='https://www.credly.com/badges/8b82addc-0047-4d79-a5c3-9d55235cbb95/public_url'target='_blank'>التحقق من الشارة</a>
                         <span>اقلب البطاقة لإظهار المزيد من المعلومات</span>
@@ -161,7 +154,7 @@ const Card = ({lan}) => {
                     <div className="card-cert-front" dir='rtl'>
                         <h2>مسك للإعداد المهني</h2>
                         
-                        <p>إتقان مهارات مثل الاتصال وتخطيط المشاريع وإدارة الوقت</p>
+                        
                         <p>مقدمة من : Misk</p>
                         <a className='badge' href='https://www.credential.net/e2fce05f-25cb-40cf-9018-29ff6af565b2'target='_blank'>التحقق من الشارة</a>
 
